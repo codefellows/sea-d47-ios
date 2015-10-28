42ba6f246625ec49dfabc771efe688be2304947a
Duncan Marsh <dwolfm@gmail.com>
2015-10-28 14:51:58 -0700

    added oct-28 titter-clone lecture code

---
+++ week-1/oct-28-twitter-clone/twitter-clone/TwitterCF/TwitterCF/TimelineCell.swift
@@ -0,0 +1,21 @@
+//
+//  TimelineCell.swift
+//  
+//
+//  Created by Jeffrey Jacka on 8/6/15.
+//
+//
+
+import UIKit
+
+class TimelineCell: TweetCell {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
