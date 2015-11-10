00bc05ba6f61cee5223d7dd2413e9c7abc1ebfb2
Adam Wallraff <adam.wallraff@gmail.com>
2015-11-10 15:47:15 -0800

    Updated KeychainService code

---
+++ week-3/nov-10-github/lecture-code/KeychainDemo/KeychainDemo/KeyChainService.swift
@@ -0,0 +1,54 @@
+import UIKit
+import Security
+
+// Identifiers
+let userAccount = "github"
+let accessGroup = "MyService"
+
+// Arguments for the keychain queries
+let kSecClassValue = kSecClass as NSString
+let kSecAttrAccountValue = kSecAttrAccount as NSString
+let kSecValueDataValue = kSecValueData as NSString
+let kSecClassGenericPasswordValue = kSecClassInternetPassword as NSString
+let kSecAttrServiceValue = kSecAttrService as NSString
+let kSecMatchLimitValue = kSecMatchLimit as NSString
+let kSecReturnDataValue = kSecReturnData as NSString
+let kSecMatchLimitOneValue = kSecMatchLimitOne as NSString
+
+class KeychainService: NSObject {
+  
+   class func save(data: NSString) {
+    var dataFromString: NSData = data.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!
+    
+    var keychainQuery: NSMutableDictionary = NSMutableDictionary(objects: [kSecClassGenericPasswordValue, userAccount, dataFromString], forKeys: [kSecClassValue, kSecAttrAccountValue, kSecValueDataValue])
+    
+    // Delete any existing items
+    SecItemDelete(keychainQuery)
+    
+    // Add the new keychain item
+    var status: OSStatus = SecItemAdd(keychainQuery, nil)
+  }
+  
+   class func loadFromKeychain() -> NSString? {
+    // Instantiate a new default keychain query
+    // Tell the query to return a result
+    // Limit our results to one item
+    
+    var keychainQuery: NSMutableDictionary = NSMutableDictionary(objects: [kSecClassGenericPasswordValue, userAccount, kCFBooleanTrue, kSecMatchLimitOneValue], forKeys: [kSecClassValue, kSecAttrAccountValue, kSecReturnDataValue, kSecMatchLimitValue])
+    
+    var dataTypeRef :AnyObject?
+    
+    // Search for the keychain items
+    let status: OSStatus = SecItemCopyMatching(keychainQuery, &dataTypeRef)
+    
+    var contentsOfKeychain: NSString?
+    
+    if let retainedData = dataTypeRef as? NSData {
+      contentsOfKeychain = NSString(data: retainedData, encoding: NSUTF8StringEncoding)
+    } else {
+      print("Nothing was retrieved from the keychain. Status code \(status)")
+    }
+    //dataTypeRef?.release()
+    return contentsOfKeychain
+  }
+}
